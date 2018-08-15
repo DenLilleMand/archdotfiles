08a0f0254e6131444a368391b64ffb0c2adf8a63
Matti Andreas Nielsen <matti@denlillemand.com>
2018-08-15 10:21:47 +0200

    vimrc, setup.sh, i3config

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@ if [ "$1" = "home" ]; then
     xrandr --output HDMI1 --auto --output eDP1 --off --output DP2 --auto  --right-of HDMI1
 elif [ "$1" = "work" ]; then
     xrandr --output DP1 --auto --output DP2 --right-of DP1 --auto --output eDP1 --off 
+elif [ "$1" = "omnio" ]; then
+    xrandr --output DP1 --auto --output eDP1 --off 
 elif [ "$1" = "laptop" ]; then
     xrandr --output HDMI1 --off --output eDP1 --auto --output DP2 --off  
 else
