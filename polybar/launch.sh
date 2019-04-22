83cb2e05bf45fb92b84c55361ed4e4a3b7d8aed7
Matti Andreas Nielsen <matti@denlillemand.com>
2019-04-22 18:00:26 +0200

    polybar: Turn the bar on at every available screen
    Why:
    
    * Just so that it works on all setups automatically, it is a pain to
      call things manually

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,9 +4,17 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar uptop &
+# polybar uptop &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload uptop &
+  done
+else
+  polybar --reload uptop &
+fi
 
 echo "Bar launched..."
